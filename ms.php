999b789bac1776a2725154edd93b01c375ff2032
Mohammed Zuhayr Hussain <64800651+zuhayr24@users.noreply.github.com>
2023-04-25 21:47:17 +0530

    Add files via upload

---
+++ ms.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+$reg = $_SESSION['username'];
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "example";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+$fromd = $_POST["fromd"];
+$tod = $_POST["tod"];
+$days = $_POST["days"];
+$des=$_POST["dest"];
+$reason = $_POST["reason"];
+
+// Check if the values of fromd and tod already exist in the database
+$sql = "SELECT * FROM outpass WHERE reg='$reg' AND fromd='$fromd' AND tod='$tod'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "You have already requested for outpass so please wait!!";
+} else {
+    // Insert data into database
+    $sql = "INSERT INTO outpass (reg,fromd,tod,days,dest,reason) VALUES ('$reg','$fromd', '$tod','$days','$des','$reason')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "successfully recorded";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+
+?>
---
+++ studentstat.php
@@ -0,0 +1,41 @@
+<?php
+// Database configuration
+$host = "localhost"; // Replace with your host name
+$username = "root"; // Replace with your database username
+$password = ""; // Replace with your database password
+$dbname = "example"; // Replace with your database name
+
+// Create a connection to the database
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+// Check if the connection was successful
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Query the database to get the data you want to display
+$sql = "SELECT Id,status,response FROM outpass";
+$result = mysqli_query($conn, $sql);
+
+// Check if any rows were returned
+if (mysqli_num_rows($result) > 0) {
+  // Create a table to display the data
+  echo "<table>";
+  echo "<tr><th>ID</th><th>Status</th><th>Response</th></tr>";
+  
+  // Loop through each row and display the data
+  while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row["Id"] . "</td>";
+    echo "<td>" . $row["status"] . "</td>";
+    echo "<td>" . $row["response"]. "</td>";
+    echo "</tr>";
+  }
+  echo "</table>";
+} else {
+  echo "No data found";
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
